d1cdc8ea9cf38124dcbe9580843140f384fa2fb3
Ayushman00 <134496359+Ayushman00@users.noreply.github.com>
2023-05-24 18:56:15 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+ float  centigrade,fahrenheit;
+ printf("Enter the temperature in centigrade\n");
+ scanf("%f",&centigrade);
+ fahrenheit=(9*centigrade)/5+32;
+ printf("centigrade to fahrenheit=%f\n", fahrenheit);
+
+ return 0;
+  }
