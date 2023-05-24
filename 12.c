5817a90c38718b6fd4dee689517dc30c311e9800
Ayushman00 <134496359+Ayushman00@users.noreply.github.com>
2023-05-24 23:16:34 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+ int n;  
+printf("Enter the number ");
+scanf("%d",&n);
+int sum=0;
+for (int i=0;i<=n;i++)
+{include
+sum=sum+i;
+}
+printf("The sum is %d",sum);
+ return 0;
+  }
