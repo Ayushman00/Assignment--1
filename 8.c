646c3ff2eaed5ae48aa5d6b5b5c2798e64d304ed
Ayushman00 <134496359+Ayushman00@users.noreply.github.com>
2023-05-24 19:52:32 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int a;
+printf("Enter the value of a  :-\n");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d is even number",a);
+}
+else
+{
+printf("%d is odd number",a);
+}
+ return 0;
+  }
