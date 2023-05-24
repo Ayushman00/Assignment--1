9c2b1fff6ed207557e2494f14010a882da1606d3
Ayushman00 <134496359+Ayushman00@users.noreply.github.com>
2023-05-24 18:07:55 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+int main()
+{
+ float  radius ,area , circumference;
+ printf(" Enter radius ");
+ scanf("%f",&radius);
+ area = 3.14*radius*radius;
+ circumference=2*3.14*radius;
+  printf("The area and circumference of circle  are =%f %f",area, circumference);
+ 
+ return 0;
+  }
