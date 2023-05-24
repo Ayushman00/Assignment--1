2a6c174723e18f67895f61d6b7c46a175ff6424e
Ayushman00 <134496359+Ayushman00@users.noreply.github.com>
2023-05-24 19:07:16 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+
+int a,b;
+printf("Enter the value of a and b :-\n");
+scanf("%d%d",&a,&b);
+ if(a==b)
+{
+printf("The numbers are equal");
+}
+else
+printf("The numbers are not equal");
+ return 0;
+  }
